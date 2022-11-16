ab421be92470c937174d58a255d596b71be362de
usama <usama.anwarbutt.1@gmail.com>
2022-11-16 11:52:10 +0500

    factory design pattern

---
+++ src/design_patterns/factory_design_pattern/CommericalPlan.java
@@ -0,0 +1,9 @@
+package design_patterns.factory_design_pattern;
+
+class CommercialPlan extends Plan {
+    // @override
+    public void getRate() {
+        rate = 7.50;
+    }
+}
+// end of CommercialPlan class.
---
+++ src/design_patterns/factory_design_pattern/InstitutionalPlan.java
@@ -0,0 +1,8 @@
+package design_patterns.factory_design_pattern;
+
+class InstitutionalPlan extends Plan {
+    // @override
+    public void getRate() {
+        rate = 5.50;
+    }
+}// end of InstitutionalPlan class.
